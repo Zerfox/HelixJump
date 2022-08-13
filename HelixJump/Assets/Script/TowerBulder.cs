c6af2cd4d17e89f8784eda9e2bf8627dec16b510
Данил <wanty3@mail.ru>
2022-08-13 19:40:02 +0300

    Добавил скрит-спаунер башни

---
+++ HelixJump/Assets/Script/TowerBulder.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBulder : MonoBehaviour
+{
+    [SerializeField] private int _lievelCount;
+    [SerializeField] private GameObject _beam;
+    [SerializeField] private SpawnPlatform _spawnPlatform;
+    [SerializeField] private Platform [] _massPlatforms;
+    [SerializeField] private FinishPlatform _finishPlatform;
+
+    private float _startAndFinishAdditionalScale = 1f;
+
+    public float BeamScaly => _lievelCount + _startAndFinishAdditionalScale;
+    private void Start()
+    {
+        Build();
+    }
+
+    private void Build()
+    {
+        GameObject beam = Instantiate(_beam, transform);
+        beam.transform.localScale = new Vector3(1, BeamScaly, 1);
+
+        Vector3 spawnPosition = beam.transform.position;
+        spawnPosition.y += beam.transform.localScale.y;
+
+        SpawnPlatform(_finishPlatform, ref spawnPosition, beam.transform);
+
+        for (int i = 0; i < _lievelCount; i++)
+        {
+            SpawnPlatform(_massPlatforms[Random.Range(0, _massPlatforms.Length)], ref spawnPosition, beam.transform);
+        }
+        
+        SpawnPlatform(_finishPlatform, ref spawnPosition, beam.transform);
+    }
+
+    private void SpawnPlatform (Platform platform, ref Vector3 spawnPosition, Transform parent)
+    {
+        Instantiate(platform, spawnPosition, Quaternion.Euler(0, Random.Range(0, 360), 0), parent);
+        spawnPosition.y -= 1;
+    }
+}
